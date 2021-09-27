42f89a53abc0653c3d05cbfa800412c6a74f4ce1
Daniel <d.andres.bacho@gmail.com>
2021-09-27 09:52:39 +0200

    Create Login Screen

--- app/src/main/java/com/example/eventlist/Activities/MainActivity.kt
+++ app/src/main/java/com/example/eventlist/Activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.eventlist
+package com.example.eventlist.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eventlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
