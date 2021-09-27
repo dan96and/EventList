7325ae110367a92e5fee97b600b2664866d45627
Daniel <d.andres.bacho@gmail.com>
2021-09-27 13:35:24 +0200

    Screen Login

--- app/src/main/java/com/example/eventlist/Fragments/LoginView.kt
+++ app/src/main/java/com/example/eventlist/Fragments/LoginView.kt
@@ -1,16 +1,21 @@
 package com.example.eventlist.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.eventlist.R
+
 
 class LoginView : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_view, container, false)
     }
 }
