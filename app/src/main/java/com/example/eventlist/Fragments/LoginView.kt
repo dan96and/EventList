6e7ed8422fd441afb617f0084adc320b8884ae32
Daniel <d.andres.bacho@gmail.com>
2021-09-27 17:39:29 +0200

    Creation of the ForgotYourPassword screen

--- app/src/main/java/com/example/eventlist/Fragments/LoginView.kt
+++ app/src/main/java/com/example/eventlist/Fragments/LoginView.kt
@@ -5,17 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.eventlist.R
+import com.example.eventlist.databinding.FragmentLoginViewBinding
 
 
 class LoginView : Fragment() {
-
+    //Declaration binding
+    private var _binding: FragmentLoginViewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_view, container, false)
+        _binding = FragmentLoginViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //lblForgotPassword Navigation
+        binding.lblForgotPassword.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_ForgotPasswordFragment)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
