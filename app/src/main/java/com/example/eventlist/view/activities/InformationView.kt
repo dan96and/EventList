ced11486811af60f2bbee2d0482c92e4741d98b1
Daniel <d.andres.bacho@gmail.com>
2021-11-04 18:29:56 +0100

    Update InformationView.kt

--- app/src/main/java/com/example/eventlist/view/activities/InformationView.kt
+++ app/src/main/java/com/example/eventlist/view/activities/InformationView.kt
@@ -3,10 +3,20 @@ package com.example.eventlist.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.eventlist.R
+import com.example.eventlist.databinding.ActivityInformationViewBinding
 
 class InformationView : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInformationViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_information_view)
+        binding = ActivityInformationViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Navigation
+        binding.btnBeforeArrow.setOnClickListener {
+            finish()
+        }
     }
 }
